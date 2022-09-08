024be8918e2c5894115fd6c71ba27cc5f023d601
dchan <dchan365@outlook.com>
2022-09-07 21:12:31 -0400

    Amend import statement and insert console log statements

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # js-snippets
+- reorder array element
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-import reorderArray './reorder-array.js';
+import reorderArray from './reorder-array.js';
 
 let fruits = ['apple', 'banana', 'kiwi', 'orange', 'strawberry', 'grape'];
 
+console.log(fruits);
 reorderArray(fruits, 1, 5);
+console.log(fruits);
