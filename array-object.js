96210c9b208d4c043c0f0d99ce5760aa29ebc427
h12chan <112553752+h12chan@users.noreply.github.com>
2022-09-11 10:50:52 -0400

    Create array-object.js

---
+++ array-object.js
@@ -0,0 +1,16 @@
+let train = [
+  { 'colour': 'green',
+   'type': 'passenger'},
+  { 'colour': 'red',
+   'type': 'passenger'},
+  { 'colour': 'yellow',
+   'type': 'passenger'},
+  { 'colour': 'brown',
+   'type': 'cargo'},
+  { 'colour': 'black',
+   'type': 'cargo'},
+  { 'colour': 'blue',
+   'type': 'engine'}
+  ]
+
+export default train;
