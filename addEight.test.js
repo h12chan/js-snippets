2c5f1cb3449d9175b507c84c176d606fa2e954ef
h12chan <112553752+h12chan@users.noreply.github.com>
2022-09-17 19:53:27 -0400

    Update addEight.test.js

--- addEight.test.js
+++ addEight.test.js
@@ -4,3 +4,8 @@ const addEight = require('./addEight');
 test('returns the number plus eight', () => {
     expect(addEight(10)).toBe(18);
 })
+
+// npm --version
+// npm init -y
+// npm install --save-dev jest
+// npm run test
