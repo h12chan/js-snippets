066371d33525c1cd2938cfc9491cab0f842189cd
h12chan <112553752+h12chan@users.noreply.github.com>
2022-09-24 16:52:06 -0400

    Update pass-by-value-reference.js

--- pass-by-value-reference.js
+++ pass-by-value-reference.js
@@ -5,4 +5,16 @@ let c = [1, 2]
 let d = c
 d = [3, 4, 5]
 
+let c = [1, 2] // 0x01
+console.log(`c = ${c}`)
+add(c, 3)
+console.log(`c = ${c}`)
+
+function add(array, element) { // 0x01, 3
+	array.push(element)
+}
+
+// primitives are pass-by-value
+// arrays, objects, classes are pass-by-reference
+
 // https://www.youtube.com/watch?v=-hBJz2PPIVE
